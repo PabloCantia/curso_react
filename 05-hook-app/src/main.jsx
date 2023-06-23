eb01f976e39d752c094792c0fc59aa023426e84c
PabloCantia <pablo.ramirez@cantia.mx>
2023-06-23 17:03:08 -0600

    useContext

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
+
 // import { HooksApp } from './HooksApp.jsx'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -8,13 +10,19 @@ import './index.css'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
-
+//import { Layout } from './05-useLayoutEffect/Layout'
+//import { Memorize } from './06-memos/Memorize'
+//import { MemoHook } from './06-memos/MemoHook'
+//import { CallbackHook } from './06-memos/CallbackHook'
+//import { Padre } from './07-tarea-memo/Padre'
+//import './08-useReducer/intro-reducer'
+//import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-    //{/* <HooksApp /> */}
-    //{/* <CounterWithCustomHook/> */}
-    <Layout />
-  //</React.StrictMode>,
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
